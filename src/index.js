7a0ec7dfc1304e02f12b8dd8c116dcba45b7f90e
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2021-03-21 18:15:10 +0530

    express server created on port 8080

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+  console.log("Listening you on " + PORT);
+});
