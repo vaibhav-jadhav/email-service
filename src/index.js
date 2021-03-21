71e35d27d477007322bf85e114ec7edf60db9713
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2021-03-21 18:18:14 +0530

    express api endpoint created for email service

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8080
 
+app.post('/emailService',(request,response)=>{
+
+})
 app.listen(PORT, () => {
   console.log("Listening you on " + PORT);
 });
