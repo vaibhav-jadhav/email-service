2c9721b1c09ca34d38f2c1d20225d1f8ce498134
vaibhav jadhav <vaibhav.jadhav.csm@gmail.com>
2021-03-21 18:58:27 +0530

    added middleware for mailgun service

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const {sendEmailUsingSendGrid} = require("./sendgrid")
+const {sendEmailUsingMailGun} = require("./mailgun")
 app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 8080
 
-app.post('/emailService',sendEmailUsingSendGrid,(request,response)=>{
-    response.json({
-      "failed ":"somwone"
-    });
-})
+app.post('/emailService',sendEmailUsingMailGun,sendEmailUsingSendGrid);
 
 app.listen(PORT, () => {
   console.log("Listening you on " + PORT);
